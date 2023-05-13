3d71b09b5d1dff0bb8eb3c8de20f8199caca56b8
SERVITRAITOR <133474842+SERVITRAITOR@users.noreply.github.com>
2023-05-13 15:10:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,60 @@
-let form = document.getElementById("form");
-let previewButton = document.getElementById("preview-button");
-let resetButton = document.getElementById("reset-button");
+// get the form elements
+const form = document.querySelector("#pdf-form");
+const previewBtn = form.querySelector("#preview-btn");
+const downloadBtn = form.querySelector("#download-btn");
+const input = form.querySelector("#input-html");
+const output = form.querySelector("#output-pdf");
 
-let tableContainer = document.getElementById("table-container");
-let fontPicker = document.getElementById("font-picker");
-let fontColorPicker = document.getElementById("font-color-picker");
-let tableColorPicker = document.getElementById("table-color-picker");
+// add event listeners to the buttons
+previewBtn.addEventListener("click", generatePDFPreview);
+downloadBtn.addEventListener("click", generatePDFDownload);
 
-previewButton.addEventListener("click", function () {
-  let tableHTML = `
-    <table>
-      <thead>
-        <tr>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Age</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>John</td>
-          <td>Doe</td>
-          <td>35</td>
-        </tr>
-        <tr>
-          <td>Jane</td>
-          <td>Smith</td>
-          <td>28</td>
-        </tr>
-        <tr>
-          <td>Bob</td>
-          <td>Johnson</td>
-          <td>42</td>
-        </tr>
-      </tbody>
-    </table>
-  `;
+// function to generate a PDF preview
+function generatePDFPreview() {
+  // create a new jspdf instance
+  const doc = new jsPDF();
 
-  tableContainer.innerHTML = tableHTML;
-  tableContainer.style.backgroundColor = tableColorPicker.value;
-  tableContainer.style.color = fontColorPicker.value;
-  tableContainer.style.fontFamily = fontPicker.value;
-});
+  // get the input html content
+  const html = input.value;
 
-resetButton.addEventListener("click", function () {
-  form.reset();
-  tableContainer.innerHTML = "";
-  tableContainer.style.backgroundColor = "";
-  tableContainer.style.color = "";
-  tableContainer.style.fontFamily = "";
-});
+  // create a canvas element to render the input html
+  const canvas = document.createElement("canvas");
+
+  // set the canvas dimensions to the document dimensions
+  canvas.width = doc.internal.pageSize.getWidth();
+  canvas.height = doc.internal.pageSize.getHeight();
+
+  // render the input html to the canvas using html2canvas
+  html2canvas(html, { canvas: canvas }).then(() => {
+    // add the canvas image as a page to the PDF document
+    doc.addImage(canvas.toDataURL(), "JPEG", 0, 0);
+
+    // set the output iframe source to the PDF document data
+    output.src = doc.output("datauristring");
+  });
+}
+
+// function to generate a PDF download
+function generatePDFDownload() {
+  // create a new jspdf instance
+  const doc = new jsPDF();
+
+  // get the input html content
+  const html = input.value;
+
+  // create a canvas element to render the input html
+  const canvas = document.createElement("canvas");
+
+  // set the canvas dimensions to the document dimensions
+  canvas.width = doc.internal.pageSize.getWidth();
+  canvas.height = doc.internal.pageSize.getHeight();
+
+  // render the input html to the canvas using html2canvas
+  html2canvas(html, { canvas: canvas }).then(() => {
+    // add the canvas image as a page to the PDF document
+    doc.addImage(canvas.toDataURL(), "JPEG", 0, 0);
+
+    // download the PDF document
+    doc.save("output.pdf");
+  });
+}
